c6c57f1b19be85f6909b596accef28b3566626df
SYLVESTER DARIUS <silverstar.381@icloud.com>
2026-05-08 13:40:27 +0300

    first commit

---
+++ v.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 16;
+        String name = "John";
+
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
